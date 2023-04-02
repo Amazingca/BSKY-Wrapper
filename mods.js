3148ed5e80be8bb0a4ad82d4c80889f2fa60831e
Amazingca <67606943+Amazingca@users.noreply.github.com>
2023-04-02 12:27:30 -0600

    Update mods.js

--- mods.js
+++ mods.js
@@ -155,10 +155,10 @@ export function reactorContructor(postObj, amounts) {
     
     postObj.replyCount = amounts[0][0];
     postObj.repostCount = amounts[1][0];
-    postObj.upvoteCount = amounts[2][0];
+    postObj.likeCount = amounts[2][0];
     
     postObj.viewer.repost = amounts[1][1];
-    postObj.viewer.upvote = amounts[2][1];
+    postObj.viewer.like = amounts[2][1];
   }
   
   var upvoteE;
@@ -178,17 +178,17 @@ export function reactorContructor(postObj, amounts) {
       </flex>`;
   }
   
-  if (postObj.viewer.upvote != undefined) {
+  if (postObj.viewer.like != undefined) {
     upvoteE = 
       `<flex class="reactors">
         <svg class="upvote_${postObj.uri}-${postObj.cid}" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 16 16" width="20" height="20"><path d="M7.655 14.916v-.001h-.002l-.006-.003-.018-.01a22.066 22.066 0 0 1-3.744-2.584C2.045 10.731 0 8.35 0 5.5 0 2.836 2.086 1 4.25 1 5.797 1 7.153 1.802 8 3.02 8.847 1.802 10.203 1 11.75 1 13.914 1 16 2.836 16 5.5c0 2.85-2.044 5.231-3.886 6.818a22.094 22.094 0 0 1-3.433 2.414 7.152 7.152 0 0 1-.31.17l-.018.01-.008.004a.75.75 0 0 1-.69 0Z"></path></svg>
-        <div class="reactor">${postObj.upvoteCount}</div>
+        <div class="reactor">${postObj.likeCount}</div>
       </flex>`;
   } else {
     upvoteE = 
       `<flex class="reactors">
         <svg class="upvote_${postObj.uri}-${postObj.cid}" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 16 16" width="20" height="20"><path d="m8 14.25.345.666a.75.75 0 0 1-.69 0l-.008-.004-.018-.01a7.152 7.152 0 0 1-.31-.17 22.055 22.055 0 0 1-3.434-2.414C2.045 10.731 0 8.35 0 5.5 0 2.836 2.086 1 4.25 1 5.797 1 7.153 1.802 8 3.02 8.847 1.802 10.203 1 11.75 1 13.914 1 16 2.836 16 5.5c0 2.85-2.045 5.231-3.885 6.818a22.066 22.066 0 0 1-3.744 2.584l-.018.01-.006.003h-.002ZM4.25 2.5c-1.336 0-2.75 1.164-2.75 3 0 2.15 1.58 4.144 3.365 5.682A20.58 20.58 0 0 0 8 13.393a20.58 20.58 0 0 0 3.135-2.211C12.92 9.644 14.5 7.65 14.5 5.5c0-1.836-1.414-3-2.75-3-1.373 0-2.609.986-3.029 2.456a.749.749 0 0 1-1.442 0C6.859 3.486 5.623 2.5 4.25 2.5Z"></path></svg>
-        <div class="reactor">${postObj.upvoteCount}</div>
+        <div class="reactor">${postObj.likeCount}</div>
       </flex>`;
   }
 
