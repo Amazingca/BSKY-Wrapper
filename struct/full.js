7c150cba0ce734fc13efe7c3a027f0d2c15081bf
Amazingca <67606943+Amazingca@users.noreply.github.com>
2023-04-02 13:13:54 -0600

    Update full.js

--- struct/full.js
+++ struct/full.js
@@ -102,7 +102,7 @@ export async function postFull(userId, post, type) {
     
     var postText;
     
-    if (postObj.record.facets != undefined) {
+    if (postObj.record.value.facets != undefined) {
       
       var selection = [];
 
