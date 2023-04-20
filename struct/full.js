aeb043bc47f27fcf8ab153b6e7185f99d6dc10b7
Amazingca <67606943+Amazingca@users.noreply.github.com>
2023-04-19 23:05:32 -0600

    Update full.js

--- struct/full.js
+++ struct/full.js
@@ -144,8 +144,10 @@ export async function postFull(userId, post, type) {
 
           for (var o = 0; o < mentionLength; o++) {
 
-            selection[i][1] = selection[i][1] + postObj.record.text.split("")[o + startPos];
+            selection[i][1] = selection[i][1] + postObj.record.text.split("")[o + startPos - 2];
           }
+          
+          //console.log(selection[i][1], selection[i][1].split("").length, mentionLength);
         }
       }
       
