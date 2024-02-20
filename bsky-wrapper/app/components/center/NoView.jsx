959aef22dc3b857373a8130532dc2afffd8631b5
Amazingca <67606943+Amazingca@users.noreply.github.com>
2024-02-20 02:26:13 -0700

    Minor display change

--- bsky-wrapper/app/components/center/NoView.jsx
+++ bsky-wrapper/app/components/center/NoView.jsx
@@ -1,7 +1,7 @@
 const NoView = () => {
 
     return (
-        <div className={"Post"}>
+        <div className={"Post NoView"}>
             <p>The record you are trying to view does not exist or is not publicly available.</p>
         </div>
     )
