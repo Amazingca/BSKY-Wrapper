6637693303d1ddd5e62df0eb5a16e966424725ae
Amazingca <autumn@amazingca.dev>
2025-03-11 11:51:41 -0600

    Update links and references, copyright date

--- bsky-wrapper/app/components/footerbar/Footer.jsx
+++ bsky-wrapper/app/components/footerbar/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     return (
         <div className="Footer">
             <footer>Open-sourced on <a href="https://github.com/Amazingca/BSKY-Wrapper" target="_blank">Github</a>.</footer>
-            <footer>&copy; 2024 by its maintainers</footer>
+            <footer>&copy; 2025 by its maintainers</footer>
             <footer><Link to={`/about`}>About</Link> · <Link to={`/acknowledgments`}>Acknowledgments</Link></footer>
         </div>
     )
